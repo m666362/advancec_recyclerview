4aba03f798632ba50d5c7a93b6bf412f9a3e4251
github_username <mdrayhan9464@gmail.com>
2021-04-20 07:11:08 +0600

    initial commit

---
+++ app/src/main/java/org/richit/advancedrecyclerview/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package org.richit.advancedrecyclerview.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.richit.advancedrecyclerview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
